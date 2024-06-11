16c3e43d534c043c4d86ce2c0f10bb9c90e1a9eb
JugadsinghReal <143512296+JugadsinghReal@users.noreply.github.com>
2024-06-11 20:38:47 +0200

    t

--- js/kamer4/main4.js
+++ js/kamer4/main4.js
@@ -2,7 +2,7 @@ const winnerroom = document.querySelector(".goToWin")
 winnerroom.addEventListener("click", function(){
    let finalanwser = prompt("is walvis een vis of een zoogdier?")
    if (finalanwser == "zoogdier"){
-    window.location.href = ("/WinorLose/win-page.html")
+    window.location.href = ("../WinorLose/win-page.html")
    }
    else{
     alert("incorrect...")
